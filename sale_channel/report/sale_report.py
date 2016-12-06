ef8ceae8a275b5665d26c2ae3a15987f5c0f617a
mccwdev <lennart@1200wd.com>
2016-12-06 14:00:37 +0100

    [REF] Update headers, make freely available

--- sale_channel/report/sale_report.py
+++ sale_channel/report/sale_report.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #    Sales Channels
-#    Copyright (C) 2016 June
-#    1200 Web Development
-#    http://1200wd.com/
+#    © 2016 - 1200 WebDevelopment <http://1200wd.com/>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
