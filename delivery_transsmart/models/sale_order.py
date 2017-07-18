c3f5b219f271777d2ed99fcad0e3971e78aa510e
Ronald Portier <ronald@therp.nl>
2017-07-18 13:20:57 +0200

    [FIX] Improve transsmart logging.

--- delivery_transsmart/models/sale_order.py
+++ delivery_transsmart/models/sale_order.py
@@ -1,24 +1,13 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Delivery Transsmart Ingegration
-#    © 2016 - 1200 Web Development <http://1200wd.com/>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016-2017 1200 Web Development <http://1200wd.com/>
+# © 2017 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import logging
+
 from openerp import models, fields
+
+
+_logger = logging.getLogger(__name__)
 
 
 class SaleOrder(models.Model):
@@ -39,12 +28,16 @@ class SaleOrder(models.Model):
         result = super(SaleOrder, self).action_ship_create(
             cr, uid, ids, context=context
         )
-        try:
-            # Should test beforehand wether transsmart applies. For
-            # the moment surpress warning. Transsmart will be unlinked from
-            # sales_order anyway:
-            for sale in sales:
+        for sale in sales:
+            try:
+                # Should test beforehand wether transsmart applies. For
+                # the moment surpress warning. Transsmart will be unlinked
+                # from sales_order anyway:
                 sale.picking_ids.action_get_transsmart_rate()
-        except:
-            pass
+            except:
+                _logger.info(
+                    "Sale order %s not valid for transsmart" %
+                    sale.display_name
+                )
+                pass
         return result
