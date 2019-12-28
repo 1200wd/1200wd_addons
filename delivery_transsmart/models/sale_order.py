2fb269e012760339e0d1254905f6259830c6fac0
Ronald Portier <ronald@therp.nl>
2019-12-28 22:22:45 +0100

    [FIX] delivery_transsmart. Restore cost_center on sale_order.

--- delivery_transsmart/models/sale_order.py
+++ delivery_transsmart/models/sale_order.py
@@ -11,13 +11,19 @@ class SaleOrder(models.Model):
         comodel_name='delivery.service.level.time',
         oldname='service_level_time_id',
     )
+    cost_center_id = fields.Many2one(
+        comodel_name='transsmart.cost.center',
+        string='Delivery Cost Center',
+        readonly=True,
+    )
 
     @api.multi
     def action_ship_create(self):
-        """Set transsmart information on picking from service level."""
+        """Set transsmart information on picking from service level and cost center."""
         result = super(SaleOrder, self).action_ship_create()
         for this in self:
             this.picking_ids.write({
                 'delivery_service_level_time_id': this.delivery_service_level_time.id,
+                'cost_center_id': this.cost_center_id.id,
             })
         return result
