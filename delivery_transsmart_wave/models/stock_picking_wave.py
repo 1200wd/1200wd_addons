07aa48903f08f6ed4eca5fff7520dd87e79bb771
Ronald Portier <ronald@therp.nl>
2019-12-31 12:29:50 +0100

    [FIX] #4801 Remove unneeded transsmart_confirmed field.

--- delivery_transsmart_wave/models/stock_picking_wave.py
+++ delivery_transsmart_wave/models/stock_picking_wave.py
@@ -7,21 +7,6 @@ from openerp import api, fields, models
 class StockPickingWave(models.Model):
     _inherit = "stock.picking.wave"
 
-    transsmart_confirmed = fields.Boolean(
-        "Transsmart Confirmed",
-        compute="_compute_transsmart_confirmed",
-        store=False,
-    )
-
-    @api.multi
-    def _compute_transsmart_confirmed(self):
-        for rec in self:
-            if rec.picking_ids:
-                rec.transsmart_confirmed = all(
-                    rec.picking_ids.mapped('carrier_tracking_ref'))
-            else:
-                rec.transsmart_confirmed = False
-
     @api.multi
     def action_transsmart_book_shipping(self):
         for rec in self:
