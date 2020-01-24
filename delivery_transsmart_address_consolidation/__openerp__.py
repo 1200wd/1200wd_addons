4ddc88c570ade7a45054cf0e9b7b214efe2b0103
Ronald Portier <ronald@therp.nl>
2020-01-24 11:49:23 +0100

    [FIX] delivery_transsmart_address_consolidation. Update.
    Change this module to be compatible with changes in delivery_transsmart.

--- delivery_transsmart_address_consolidation/__openerp__.py
+++ delivery_transsmart_address_consolidation/__openerp__.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015 ONESTEiN BV <http://www.onestein.nl>
 # Copyright 2016 1200 Web Development <https://1200wd.com/>
-# Copyright 2019 Therp BV <https://therp.nl>
+# Copyright 2019-2020 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': "Transsmart Address Consolidation",
     'summary': """Address Consolidation support for Transsmart integration""",
     'description': """
-    Integrates delivery_transsmart with address_consolidation
+    Integrates delivery_transsmart with address_consolidation.
+
     Uses the denormalized address from address_consolidation in all
     stock_transsmart communication instead off the standard
     res_partner_address.
-
-    This module is based on Onestein's version
-    but is compatible with the new Transsmart API
-    and has many bug fixes and improvements.
     """,
     'author': "1200 Web Development",
-    'website': "http://1200wd.com",
+    'website': "https://github.com/1200wd/1200wd_addons",
     'category': 'Warehouse',
     'version': '8.0.2.0',
     'depends': [
--- delivery_transsmart_address_consolidation/models/stock_picking.py
+++ delivery_transsmart_address_consolidation/models/stock_picking.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015 ONESTEiN BV <http://www.onestein.nl>
-# Copyright 2016 1200 Web Development <https://1200wd.com/>
-# Copyright 2019 Therp BV <https://therp.nl>
+# Copyright 2019-2020 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from openerp import models
 
@@ -9,8 +7,8 @@ from openerp import models
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    def action_transsmart_book_shipping(self):
-        document = super(StockPicking, self).action_transsmart_book_shipping()
+    def get_transsmart_shipping_document(self):
+        document = super(StockPicking, self).get_transsmart_shipping_document()
         document['addresses'][1].update({
             "name": self.partner_id.name or '',
             "addressLine1": self.shipping_partner_street or '',
